c0446bf8667fef2e8c90c930373bb8052b8cb527
Valeriy Pogorelov <valeriy.pogorelov@together.com>
2024-12-17 18:44:50 +0200

    Improve prompt

--- prompts/docks/tsDocsPromptForGpt4.ts
+++ prompts/docks/tsDocsPromptForGpt4.ts
@@ -20,7 +20,8 @@ Analyze the provided .ts file and add inline documentation. Follow these rules s
 * ALL prop may have comment. If property hasn't a comment, add a comment for it.
 * Use one-line comments for each property to describe its purpose and behavior if it's clear.
 * Use two-line comments for complex properties that require detailed explanations.
-* Max line count for prop comment is 2 lines.
+* Max line count for added prop comment is 2 lines.
+* If you see existed comment for prop with useful information for it using, save this information in the comment.
 * For optional properties explain the behavior when provided or omitted.
 * For other types: Clearly explain the purpose and expected value.
 * Comment should be in format /** ... */
@@ -28,6 +29,23 @@ Analyze the provided .ts file and add inline documentation. Follow these rules s
 * Skip comments for prop named "children".
 * Add only comments but not empty lines before the prop.
 * Skip obvious phrases like  "Optional; may be omitted if no action is required upon change."
+
+### Examples of existing useful comments, that should be saved:
+* <<</**
+   * Used for disabling css transition on button
+   * By default it's true, that mean transition is enabled
+   * Now used only for split 45+ experiment
+   * Used in two places TARGET_USER_INFO_GALLERY_STICKED and TARGET_USER_INFO_GALLERY
+   * @see UserActions.js
+   */>>>
+* <<</**
+   * Used together with "fullWidth" property. Implements logic, when on mobile
+   * environment button is full width, and on web - it should be min. 200px wide.
+   * Also, can be used for other "adaptation" things
+   */>>>
+* <<</**
+   * Used together with "form"
+   */>>>
 
 # Requirements for the code modifications absence:
 * Retain the structure of the original code.
@@ -52,7 +70,7 @@ Analyze the provided .ts file and add inline documentation. Follow these rules s
 
 # Examples of input and output:
 
-Example Input:
+Example 1 Input:
 <<<typescript
 import React, {FC} from 'react';
 
@@ -86,7 +104,7 @@ export default TileLayerFactory;
 
 >>>
 
-Example Output:
+Example 1 Output:
 <<<typescript
 import React, {FC} from 'react';
 
@@ -130,6 +148,343 @@ const TileLayerFactory: FC<TileLayerFactoryProps> = ({
 };
 
 export default TileLayerFactory;
+
+>>>
+
+Example 2 Input:
+<<<typescript
+import React, {
+  FC,
+  ReactNode,
+  Children,
+  isValidElement,
+  cloneElement,
+  ReactElement,
+} from 'react';
+import cn from 'classnames';
+import get from 'lodash/get';
+
+import Align from '../../constants/Align';
+import SpacingSize from '../../constants/SpacingSize';
+import SpacingDirection from '../../constants/SpacingDirection';
+import BarPosition from '../../constants/BarPosition';
+import {Bar} from '../card';
+import {Spacing} from '../spacing';
+import Action, {ActionProps} from './Action';
+import css from './Actions.css';
+
+export interface ActionsProps {
+  children: ReactNode;
+  className?: string;
+  spacing?: SpacingSize;
+  withBar?: boolean; // @todo Useless property. Remove it together with 'position' and 'inverse'
+  inverse?: boolean;
+  vertical?: boolean;
+  fullWidth?: boolean;
+  stretch?: boolean;
+  align?: Align;
+  // Used only together with prop 'withBar'
+  position?: BarPosition;
+  itemSpacing?: SpacingSize;
+  reverse?: boolean;
+}
+
+const renderSpacedActions = (
+  spacing: SpacingSize,
+  oneSidedMargin: boolean,
+  actions: JSX.Element,
+) => {
+  if (spacing === SpacingSize.NONE) return actions;
+
+  return (
+    <Spacing
+      size={spacing}
+      direction={
+        oneSidedMargin ? SpacingDirection.VERTICAL : SpacingDirection.BOTH
+      }
+      oneSidedMargin={oneSidedMargin}
+      adaptive={false}
+    >
+      {actions}
+    </Spacing>
+  );
+};
+
+const renderActions = (
+  children: ReactNode,
+  vertical: boolean,
+  fullWidth: boolean,
+  align: string | number,
+  className: string,
+  itemSpacing: SpacingSize,
+  reverse: boolean,
+  stretch: boolean,
+) => {
+  const mappedChildren = Children.map(
+    children,
+    (child: ReactElement<ActionProps>) => {
+      if (!isValidElement(child)) return null;
+
+      if (
+        child.type === Action ||
+        // Since UI kit is dynamically loaded and all components are wrapped in some sort of proxy component
+        // @see src/packages/dating/ui/index.js
+        get(child, 'type.type.displayName') === 'DeferredAction'
+      ) {
+        return cloneElement(child, {
+          className: cn(child.props.className, css.action),
+          spacing: itemSpacing,
+          vertical,
+        });
+      }
+
+      return (
+        <Action
+          spacing={itemSpacing}
+          vertical={vertical}
+          className={css.action}
+        >
+          {child}
+        </Action>
+      );
+    },
+  );
+
+  if (reverse) {
+    mappedChildren.reverse();
+  }
+
+  return (
+    <div
+      className={cn(
+        css.actions,
+        css[align],
+        stretch && css.stretch,
+        vertical && css.vertical,
+        reverse && css.reverse,
+        fullWidth && css.fullWidth,
+        className,
+      )}
+    >
+      {mappedChildren}
+    </div>
+  );
+};
+
+/**
+ * Bottom bar that shows action buttons
+ */
+const Actions: FC<ActionsProps> = ({
+  children,
+  className,
+  inverse = false,
+  spacing = SpacingSize.NONE,
+  withBar = false,
+  vertical = false,
+  fullWidth = false,
+  align = Align.RIGHT,
+  position = BarPosition.BOTTOM,
+  itemSpacing = SpacingSize.NORMAL,
+  reverse = false,
+  stretch = false,
+}) => {
+  const actions = renderActions(
+    children,
+    vertical,
+    fullWidth,
+    align,
+    className,
+    itemSpacing,
+    reverse,
+    stretch,
+  );
+
+  if (withBar) {
+    return (
+      <Bar inverse={inverse} position={position} data-test="actionsBar">
+        {renderSpacedActions(spacing, false, actions)}
+      </Bar>
+    );
+  }
+
+  return renderSpacedActions(spacing, true, actions);
+};
+
+export default Actions;
+
+>>>
+
+Example 2 Output:
+<<<typescript
+import React, {
+  FC,
+  ReactNode,
+  Children,
+  isValidElement,
+  cloneElement,
+  ReactElement,
+} from 'react';
+import cn from 'classnames';
+import get from 'lodash/get';
+
+import Align from '../../constants/Align';
+import SpacingSize from '../../constants/SpacingSize';
+import SpacingDirection from '../../constants/SpacingDirection';
+import BarPosition from '../../constants/BarPosition';
+import {Bar} from '../card';
+import {Spacing} from '../spacing';
+import Action, {ActionProps} from './Action';
+import css from './Actions.css';
+
+export interface ActionsProps {
+  children: ReactNode;
+  /** Additional class names for styling the component. */
+  className?: string;
+  /** Size of the spacing between actions. */
+  spacing?: SpacingSize;
+  /** Determines if a bar should be displayed with the actions. */
+  withBar?: boolean; // @todo Useless property. Remove it together with 'position' and 'inverse'
+  /** Inverts the color scheme of the bar, if used. */
+  inverse?: boolean;
+  /** Arranges actions vertically if true. */
+  vertical?: boolean;
+  /** Expands actions to fill the available width. */
+  fullWidth?: boolean;
+  /** Stretches actions to occupy the full height or width. */
+  stretch?: boolean;
+  /** Alignment of the actions within the container. */
+  align?: Align;
+  /** Position of the bar when 'withBar' is true. */
+  position?: BarPosition;
+  /** Spacing between individual action items. */
+  itemSpacing?: SpacingSize;
+  /** Reverses the order of the actions. */
+  reverse?: boolean;
+}
+
+const renderSpacedActions = (
+  spacing: SpacingSize,
+  oneSidedMargin: boolean,
+  actions: JSX.Element,
+) => {
+  if (spacing === SpacingSize.NONE) return actions;
+
+  return (
+    <Spacing
+      size={spacing}
+      direction={
+        oneSidedMargin ? SpacingDirection.VERTICAL : SpacingDirection.BOTH
+      }
+      oneSidedMargin={oneSidedMargin}
+      adaptive={false}
+    >
+      {actions}
+    </Spacing>
+  );
+};
+
+const renderActions = (
+  children: ReactNode,
+  vertical: boolean,
+  fullWidth: boolean,
+  align: string | number,
+  className: string,
+  itemSpacing: SpacingSize,
+  reverse: boolean,
+  stretch: boolean,
+) => {
+  const mappedChildren = Children.map(
+    children,
+    (child: ReactElement<ActionProps>) => {
+      if (!isValidElement(child)) return null;
+
+      if (
+        child.type === Action ||
+        // Since UI kit is dynamically loaded and all components are wrapped in some sort of proxy component
+        // @see src/packages/dating/ui/index.js
+        get(child, 'type.type.displayName') === 'DeferredAction'
+      ) {
+        return cloneElement(child, {
+          className: cn(child.props.className, css.action),
+          spacing: itemSpacing,
+          vertical,
+        });
+      }
+
+      return (
+        <Action
+          spacing={itemSpacing}
+          vertical={vertical}
+          className={css.action}
+        >
+          {child}
+        </Action>
+      );
+    },
+  );
+
+  if (reverse) {
+    mappedChildren.reverse();
+  }
+
+  return (
+    <div
+      className={cn(
+        css.actions,
+        css[align],
+        stretch && css.stretch,
+        vertical && css.vertical,
+        reverse && css.reverse,
+        fullWidth && css.fullWidth,
+        className,
+      )}
+    >
+      {mappedChildren}
+    </div>
+  );
+};
+
+/**
+ * Bottom bar that shows action buttons.
+ * It can optionally include a bar and arrange actions vertically or horizontally.
+ */
+const Actions: FC<ActionsProps> = ({
+  children,
+  className,
+  inverse = false,
+  spacing = SpacingSize.NONE,
+  withBar = false,
+  vertical = false,
+  fullWidth = false,
+  align = Align.RIGHT,
+  position = BarPosition.BOTTOM,
+  itemSpacing = SpacingSize.NORMAL,
+  reverse = false,
+  stretch = false,
+}) => {
+  const actions = renderActions(
+    children,
+    vertical,
+    fullWidth,
+    align,
+    className,
+    itemSpacing,
+    reverse,
+    stretch,
+  );
+
+  if (withBar) {
+    return (
+      <Bar inverse={inverse} position={position} data-test="actionsBar">
+        {renderSpacedActions(spacing, false, actions)}
+      </Bar>
+    );
+  }
+
+  return renderSpacedActions(spacing, true, actions);
+};
+
+export default Actions;
 
 >>>
 
